72d3f309c5d10cb7e177215525410cb997237ada
katherine <ktjli@ucdavis.edu>
2024-10-29 22:32:56 -0700

    stage 1 done

---
+++ Obscura/scripts/camera_controllers/position_lock.gd
@@ -0,0 +1,63 @@
+class_name PositionLock
+extends CameraControllerBase
+
+
+@export var box_width:float = 10.0
+@export var box_height:float = 10.0
+
+
+func _ready() -> void:
+	super()
+	position = target.position
+	
+
+func _process(delta: float) -> void:
+	if !current:
+		return
+	
+	if draw_camera_logic:
+		draw_logic()
+	
+	var tpos = target.global_position
+	var cpos = global_position
+	
+	global_position = target.position
+		
+	super(delta)
+
+
+func draw_logic() -> void:
+	var mesh_instance := MeshInstance3D.new()
+	var immediate_mesh := ImmediateMesh.new()
+	var material := ORMMaterial3D.new()
+	
+	mesh_instance.mesh = immediate_mesh
+	mesh_instance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
+	
+	var left:float = 5
+	var right:float = -5
+	var top:float = 5
+	var bottom:float = -5
+	
+	immediate_mesh.surface_begin(Mesh.PRIMITIVE_LINES, material)
+	immediate_mesh.surface_add_vertex(Vector3(2.5,0,0))
+	immediate_mesh.surface_add_vertex(Vector3(0,2.5,0))
+	
+	immediate_mesh.surface_add_vertex(Vector3(0,-2.5,0))
+	immediate_mesh.surface_add_vertex(Vector3(-2.5, 0, 0))
+	
+	immediate_mesh.surface_add_vertex(Vector3(0,0,-2.5))
+	immediate_mesh.surface_add_vertex(Vector3(0, 0, 2.5))
+	
+	immediate_mesh.surface_end()
+
+	material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	material.albedo_color = Color.WHITE
+	
+	add_child(mesh_instance)
+	mesh_instance.global_transform = Transform3D.IDENTITY
+	mesh_instance.global_position = Vector3(global_position.x, target.global_position.y, global_position.z)
+	
+	#mesh is freed after one update of _process
+	await get_tree().process_frame
+	mesh_instance.queue_free()
